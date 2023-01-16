d49d80177bfb82afe81a375e44f65e69c9af5d3e
juleni <69912179+juleni@users.noreply.github.com>
2023-01-16 16:19:29 +0100

    Added Player component to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
+import Player from "./components/Player";
 
 function App() {
-  return <div className="App">React App</div>;
+  return <div className="App"><Player /></div>;
 }
 
 export default App;
